c6d4d642309bff696563a04b17aaecae0d9641c4
SvetlanaShipovalova <90263099+SvetlanaShipovalova@users.noreply.github.com>
2024-05-26 10:20:46 +0300

    fix

--- render_podcasts.js
+++ render_podcasts.js
@@ -11,7 +11,9 @@ function renderAppSlider() {
 <div>
 <div class="podcasts_01">
 	<div class="podcast_text"><h2>Подкаст №${id}</h2>
-	"${name}"</div>
+	"${name}"
+<a href="https://www.google.ru/" class="btn btn-1 btn-sep icon-info">Полная версия подкаста</a>
+</div>
 	<video controls width="900px" class="video">
   <source src="${video}" type="video/mp4" />
 </video>
